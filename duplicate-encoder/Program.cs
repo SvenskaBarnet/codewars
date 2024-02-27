97d1aca5657bd04543beb443295e0cb8b0f59df5
= <klas_hermansson@hotmail.com>
2024-02-27 18:10:33 +0100

    Working code

---
+++ duplicate-encoder/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Kata
+{
+    public static string DuplicateEncode(string word)
+    {
+        string internalWord = string.Empty;
+        for(int i = 0; i < word.Length; i++)
+        {
+            string charToEncode = word[i].ToString().ToLower();
+            bool charExists = false;
+            for(int ii = 0; ii < word.Length; ii++)
+            {
+                string charToCompareTo = word[ii].ToString().ToLower();
+                if(i != ii && charToEncode == charToCompareTo)
+                {
+                    charExists = true;
+                }
+            }
+            if(!charExists)
+            {
+                internalWord += '(';
+            }
+            else
+            {
+                internalWord += ')';
+            }
+        }
+        return internalWord;
+    }
+}
