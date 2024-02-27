2bafa85142564708813a01fb79888a10f2504029
= <klas_hermansson@hotmail.com>
2024-02-27 20:37:43 +0100

    It's a start

---
+++ unique-in-order/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class Kata
+{
+    public static string UniqueInOrder<T>(IEnumerable<T> iterable) 
+    {
+        string retVal = string.Empty;
+        for (int i = 0; i < iterable.Count() ;i++)
+        {
+            if(i == 0)
+            {
+                retVal = iterable.ElementAt(i).ToString();
+            }
+            else if(!iterable.ElementAt(i).Equals(iterable.ElementAt(i -1)))
+            {
+                retVal += iterable.ElementAt(i).ToString();
+            }
+        }
+        return retVal; 
+    }
+}
